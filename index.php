32dad65ed060d6e0dbac3f0a42f282a97ae2a930
Omar Einea <eineao@gmail.com>
2016-01-30 14:49:26 +0300

    Link new files in index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php?>
+<html>
+	<head>
+        <!--Import Font Awesome-->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+		<!--Import materialize.css-->
+		<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+
+		<!--Import jQuery before materialize.js-->
+		<script type="text/javascript" src="js/jquery-2.2.0.js"></script>
+		<!--Import materialize.js-->
+		<script type="text/javascript" src="js/materialize.js"></script>
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <!--Setup page title and icon-->
+        <title>UOS UDC</title>
+	</head>
+
+	<body>
+	</body>
+</html>
