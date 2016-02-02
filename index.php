67d923d8abee9f76f4c1dcde6408e429f8d819b6
Omar Einea <eineao@gmail.com>
2016-02-02 13:33:24 +0300

    Try having the code separately

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                         <div class="card-content">
                             <div class="row" id="login-card">
                                 <!--Login form start-->
-                                <form class="col s12" action="" method="post">
+                                <form class="col s12" action="login.php" method="post">
                                     <div class="row">
                                         <h5 class="col s12">Sign in</h5>
                                     </div>
@@ -75,29 +75,6 @@
                     </div><!--Login card end-->
                 </div>
             </div>
-            <?php
-                $url = "https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_ValLogin";
-                $info = array('sid' => urlencode($_POST["sid"]), 'PIN' => urlencode($_POST["pin"]));
-            
-                foreach($info as $key=>$value) {$info_string .= $key.'='.$value.'&'}
-                rtrim($info_string, '&');
-            
-                $ci = curl_init();
-                curl_setopt($ci, CURLOPT_URL, $url);
-                curl_setopt($ch,CURLOPT_POST, count($info));
-                curl_setopt($ch,CURLOPT_POSTFIELDS, $info_string);
-
-                /*curl_setopt($ci, CURLOPT_FAILONERROR, true);
-                curl_setopt($ci, CURLOPT_FOLLOWLOCATION, true);
-                curl_setopt($ci, CURLOPT_AUTOREFERER, true);
-                curl_setopt($ci, CURLOPT_RETURNTRANSFER,true);
-                curl_setopt($ci, CURLOPT_TIMEOUT, 10);*/
-            
-                $page = curl_exec($ci);
-            
-                if(!$page) { exit(); }
-                echo $page;
-            ?>
         </div>
     </body>
 </html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+    $url = "https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_ValLogin";
+    $info = array('sid' => urlencode($_POST["sid"]), 'PIN' => urlencode($_POST["pin"]));
+
+    foreach($info as $key=>$value) {$info_string .= $key.'='.$value.'&'}
+    rtrim($info_string, '&');
+
+    $ci = curl_init();
+    curl_setopt($ci, CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_POST, count($info));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $info_string);
+
+    /*curl_setopt($ci, CURLOPT_FAILONERROR, true);
+    curl_setopt($ci, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ci, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ci, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ci, CURLOPT_TIMEOUT, 10);*/
+
+    $page = curl_exec($ci);
+
+    if(!$page) { exit(); }
+    echo $page;
+?>
